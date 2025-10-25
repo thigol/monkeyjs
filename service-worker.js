8c8d292843cfa7c88a8686729f1fade90e45ea28
thigol <thiago.limavasconcelos@ufrpe.br>
2025-10-25 13:19:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,21 @@
-// Cache PWA para Monkey Math
-const CACHE_NAME = "monkey-math-cache-v1";
+const CACHE_NAME = "monkey-math-cache-v3";
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./game.js",
-  "./manifest.webmanifest",
-  "./background.png",
-  "./menu_background.png",
-  "./score_background.png",
-  "./monkey_idle.png",
-  "./monkey_blink.png",
-  "./basket.png",
-  "./banana.png",
-  "./background_music.wav",
-  "./correct.wav",
-  "./wrong.ogg"
+  "./", "./index.html", "./style.css", "./game.js", "./manifest.webmanifest",
+  "./background.png", "./menu_background.png", "./score_background.png",
+  "./monkey_idle.png", "./monkey_blink.png", "./basket.png", "./banana.png",
+  "./background_music.wav", "./correct.wav", "./wrong.ogg"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+  ));
 });
